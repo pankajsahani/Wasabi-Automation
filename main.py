ab6ea956aed52d2a6f17d89d1a4765148d6a8d54
Ravi Voleti <xelese@gmail.com>
2021-02-19 11:03:58 -0500

    updated prod with main branch.

--- main.py
+++ main.py
@@ -55,12 +55,12 @@ class CreateBucketsForSelf:
     def create_connection_and_test(self, aws_access_key_id: str, aws_secret_access_key: str, region) -> bool:
         try:
             self.s3_client = client('s3',
-                                    endpoint_url='https://s3.' + region + '.wasabisys.com',
+                                    endpoint_url='https://s3.wasabibeta.com',
                                     aws_access_key_id=aws_access_key_id,
                                     aws_secret_access_key=aws_secret_access_key)
 
             self.iam_client = client('iam',
-                                     endpoint_url='https://iam.wasabisys.com',
+                                     endpoint_url='https://iam.wasabibeta.com',
                                      aws_access_key_id=aws_access_key_id,
                                      aws_secret_access_key=aws_secret_access_key,
                                      region_name='us-east-1')
